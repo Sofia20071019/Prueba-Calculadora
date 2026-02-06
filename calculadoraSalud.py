417a11e77a22af508d1dedee809c6043ae1d7778
AlejandraPaz <ramirezpazalejandra26@gmail.com>
2026-02-05 20:16:48 -0500

    Masa Corporal

--- calculadoraSalud.py
+++ calculadoraSalud.py
@@ -3,8 +3,31 @@
 def calcularIMC(peso, altura):
     """Calcula el Índice de Masa Corporal (IMC) dado el peso en kg y la altura en metros."""
 
-def calcular_masa_corporal(peso, altura):
-    """Calcula la masa corporal utilizando la fórmula: masa = peso / (altura ** 2)"""
+peso = float(input("Ingrese su peso: "))
+altura = float(input("Ingrese su altura: "))
+
+imc = peso / (altura * altura)
+
+print("Su IMC es:", imc)
+
+if imc < 16:
+    print("Estas en delgadez severa")
+elif imc < 17:
+    print("Estas en delgadez moderada")
+elif imc < 18.5:
+    print("Estas en delgadez aceptable")
+elif imc < 25:
+    print("Estas en peso normal")
+elif imc < 30:
+    print("Estas en sobrepeso")
+elif imc < 35:
+    print("Estas en obesidad tipo I")
+elif imc < 40:
+    print("Estas en obesidad tipo II")
+elif imc < 50:
+    print("Estas en obesidad tipo III (morbida)")
+else:
+    print("Estas en obesidad tipo IV (extrema)")
 
 def calcaularIMC(peso, altura):
     """Calcula el Índice de Masa Corporal (IMC) dado el peso en kg y la altura en metros."""
